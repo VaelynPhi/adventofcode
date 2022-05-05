16accb29baa86d25be26f5867b56c93558d56663
David B. Thomas <vaelynphi@gmail.com>
2022-05-05 03:30:15 -0400

    Solved both Day 01 puzzles.

--- 01.js
+++ 01.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
 
-const input = fs.readFileSync('01.input.txt', 'utf8').replace(/\r/g, '');
+const day = path.basename(__filename).replace('.js', '');
+
+const input = fs.readFileSync(`${day}.input.txt`, 'utf8').replace(/\r/g, '');
+
 const depths = input
 	.split('\n')
 	.map(v => 1*v)
@@ -32,6 +36,22 @@ const increasing = depths.map((v, i, a) => {
 	return v > a[i-1];
 });
 
-const increaseCount = increasing.filter(v => v === true).length;
+/* Methodology note:
+ Comparing sliding windows of 3 measurements.
+ For window n and window n+1, measurements will be at indices:
+ n, n+1, n+2
+    n+1, n+2, n+3
+ This means that the difference in the windows is merely the difference in n and n+3.
+ Therefore we start at the first n+3 (index 3) and compare n.
+*/
 
-console.log(increaseCount);
+const increasingWindowsOfThree = depths.map((v, i, a) => {
+	if(i < 3) return; // first comparison starts at index 3
+	return v > a[i-3];
+});
+
+const increasingCount = increasing.filter(v => v === true).length;
+const increasingWindowsOfThreeCount = increasingWindowsOfThree.filter(v => v === true).length;
+
+console.log(`Increasing: ${increasingCount}`);
+console.log(`Increasing windows of 3: ${increasingWindowsOfThreeCount}`);
